aa78989cad27dac37a63be91dde733742c643583
Diego Fernando Cuesta <diego.cuesta@merlinjobs.com>
2019-01-02 18:33:33 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Diego Fernando Cuesta Arevalo on 1/2/19.
+//  Copyright © 2019 Diego Fernando Cuesta Arevalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
